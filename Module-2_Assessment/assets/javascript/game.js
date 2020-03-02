ad3d3d35bc34be4af5e1c28b45187e63312dc89f
Bob Klemm <klem0020@stthomas.edu>
2020-03-02 17:18:36 -0600

    good aesthetics, but cant get JS to run at all

---
+++ Module-2_Assessment/assets/javascript/game.js
@@ -0,0 +1,23 @@
+/*Word Game for Module 2 Assessment 
+Bob Klemm 3/2/2020 */
+
+message.innerHTML = 'Hello world';
+wins.innerText = 0
+guesses.innerText = 20
+wordblanks.innerText = '_ _ _ _'
+letters.innerText = 'A'
+
+var message = document.querySelector('#message');
+var wins = document.querySelector('#winTotal');
+var wordblanks = document.querySelector('#wordBlanks');
+var guesses = document.querySelector('#guessRemain');
+var letters = document.querySelector('guessedLetters');
+
+
+
+var picture = document.querySelector('#composerImage');
+
+function {
+    if onclick() == true {
+    }
+}
