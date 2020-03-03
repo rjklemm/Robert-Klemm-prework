34a1c85ff764df5b6d90715083f9c3776b91416f
Bob Klemm <klem0020@stthomas.edu>
2020-03-03 17:46:32 -0600

    got the page to identify key letters

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -2,51 +2,55 @@
 Bob Klemm 3/2/2020 */
 
 // moving parts (words/images) in the page
-var image = document.querySelector('#composerImage');
-var message = document.querySelector('#message');
-var wins = document.querySelector('#winTotal');
-var wordblanks = document.querySelector('#wordBlanks');
-var guesses = document.querySelector('#guessRemain');
-var letters = document.querySelector('#guessedLetters');
-var startmessage = document.querySelector('#startmessage');
+const image = document.querySelector('#composerImage');
+const message = document.querySelector('#message');
+const wins = document.querySelector('#winTotal');
+const wordblanks = document.querySelector('#wordBlanks');
+const guesses = document.querySelector('#guessRemain');
+const letters = document.querySelector('#guessedLetters');
+const startmessage = document.querySelector('#startmessage');
+const music = document.querySelector('#nowplaying');
+
 
 // initial definitions of moving parts
 startmessage.innerText = 'Press any key to get started!';
 image.src = "assets/images/sheetmusic.jpg";
-message.innerText = 'Welcome!';
+message.innerText = 'Guess a letter.';
 wins.innerText = 0;
 guesses.innerText =  15;
 wordblanks.innerText = '';
 letters.innerText = '';
+music.innerText = '(In the Style of Classical Music!)';
 
 
-var word = 'BACH' //composer to guess
-//initialize game
-document.addEventListener('keypress', function(){wordgame(word)})
+var word = 'BACH' //composer to guess, will be random from a list later
 
-function wordgame(word) {
-    startmessage.innerText = 'Game in progress.'
-    message.innerText = 'Guess a letter.'
-    
-    let a = 0; //setup of letter blanks
-    var gaps = '';
-    while (a < word.length) {
-        gaps = gaps.concat(" _")
-        a++
-    }
-    wordblanks.innerText = gaps;
-    
+
+let a = 0; //setup of letter blanks
+var gaps = '';
+while (a < word.length) {
+    gaps = gaps.concat(" _")
+    a++
+}
+wordblanks.innerText = gaps;
+
+const letterg = function(event) {
+    var x = event.key;
+    message.innerText = 'You guessed ' + x;
+}
+
+//Event Listener for pressing letter
+document.addEventListener('keypress', letterg);
+
+const letterguess = function(event) {
     //guess of a letter
-    var x = onkeypress.which();
-    var guess = String.fromCharCode(x); 
-    message.innerText = 'Ok guess';
+    
     var b = 0;//index of letter in the word
     var c = 0; //number of times the letter appears
     if (letters.indexOf(guess) != -1) {
         letters.concat(" ",guess);
     }
     while (i < word.length) {
-        message.innerText = 'Good guess.';
         b0 = word.indexOf(guess,b);
         b = b0
         if (b != -1 && c === 0) {
@@ -61,5 +65,6 @@ function wordgame(word) {
         }
         i++   
     }
+    guesses.innerText(guesses)
+    letters.innerText(letters)
 }
-
